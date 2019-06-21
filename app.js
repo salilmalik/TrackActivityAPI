bc37e714157f385127b31b551d5ff48b07c202f6
salilmalik92@gmail.com <salilmalik92@gmail.com>
2019-06-21 16:02:13 -0700

    added routes for user and behaviour

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const bodyparser = require("body-parser");
-const userRoute = require('./routes/userapi');
+const userRoute = require("./routes/userapi");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require("./config");
 const app = express();
+const userapi = require("./routes/userapi");
+const behaviourapi = require("./routes/behaviourapi");
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
@@ -12,6 +16,20 @@ app.get("/", (req, res, next) => {
   res.json("Get called");
 });
 
-app.listen(3000, () => {
-  console.log("server started");
+app.use("/user", userapi);
+
+app.use("/behaviour", behaviourapi);
+
+mongoose.connect(config.DBConnectionString, { useNewUrlParser: true });
+
+mongoose.connection.on("open", function() {
+  console.log("Connect to database");
+});
+
+mongoose.connection.on("error", function(err) {
+  console.log(err);
+});
+
+app.listen(config.port, () => {
+  console.log("Server started on port "+ config.port);
 });
